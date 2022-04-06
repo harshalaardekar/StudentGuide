dee64e9e38f19606b2cb6d7544c60b864384cdda
harshalaardekar <harshalaardekar101@gmail.com>
2022-04-06 21:25:32 +0530

    SG deleted

--- SG/studyguide/includes/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['email']);
-    if(session_destroy()){
-        echo "<script type='text/javascript'>alert('Logged Out!!!');window.location='../index.php';</script>";
-    }
-?>
